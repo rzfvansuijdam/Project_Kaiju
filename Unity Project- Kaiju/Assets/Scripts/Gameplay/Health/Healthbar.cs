ab2502e1df84a645f297ffaf2523d3985f4fb084
rzfvansuijdam <rzfvansuijdam@gmail.com>
2023-06-07 12:55:51 +0200

    updated values for gun and removed redudent objects

--- Unity Project- Kaiju/Assets/Scripts/Gameplay/Health/Healthbar.cs
+++ Unity Project- Kaiju/Assets/Scripts/Gameplay/Health/Healthbar.cs
@@ -6,6 +6,15 @@ public class Healthbar : MonoBehaviour
     [SerializeField] private Slider slider;
     [SerializeField] private float EnemyHealth;
 
+    private void Start()
+    {
+        slider.enabled = false;
+    }
+
+    public void ActivateSlider()
+    {
+        slider.enabled = true;
+    }
 
     public void SetHealth(float health)
     {
